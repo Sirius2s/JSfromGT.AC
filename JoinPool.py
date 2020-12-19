bb45c1d8bf5bf0b0a2a7e77dbe1c918e780c6bde
Sirius2s <53803920+Sirius2s@users.noreply.github.com>
2020-12-19 21:08:40 +0800

    Create JoinPool.py

---
+++ JoinPool.py
@@ -0,0 +1,36 @@
+import requests                # 网络请求库
+import json                    # Json转换库
+import os
+
+poolurls = ['http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5lZRSjm9nn4J/',
+        'http://api.turinglabs.net/api/v1/jd/jxfactory/create/R0xgDZpumLgWjxq6jCH9Mg==/',
+        'http://api.turinglabs.net/api/v1/jd/farm/create/c9cfb4f9c5634c0dabd3a328a5230f87/',
+        'http://api.turinglabs.net/api/v1/jd/bean/create/twfaqwk2rzgw35t66avsmxlb6u/',
+        'http://api.turinglabs.net/api/v1/jd/pet/create/MTE1NDUyMjEwMDAwMDAwMzM4MTUyNDc=/']        ]
+
+def JoinPool(url):
+    rslt = requests.get(url)
+    # js = rslt.json()['message']
+    # js = json.loads(ret)
+    return rslt.json()['message']
+
+for purl in poolurls:
+    rs_text = JoinPool(purl) + '\n\n' + rs_text + '\n\n'
+    
+def serverJ(title, content):
+    if "PUSH_KEY" in os.environ:
+        sckey = os.environ["PUSH_KEY"]
+
+    if not sckey:
+        print("server酱服务的PUSH_KEY未设置!!\n取消推送")
+        return
+    # print("serverJ服务启动")
+    data = {
+        "text": title,
+        "desp": content
+    }
+    response = requests.post(f"https://sc.ftqq.com/{sckey}.send", data=data)
+    print(response.text)
+    
+serverJ("JoinPool", rs_text)    
+print(rs_text)

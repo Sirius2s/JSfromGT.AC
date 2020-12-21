4c4c68080f7c8541ff1320fa0a7b399c668a59ef
Sirius2s <Sirius2s@gmail.com>
2020-12-21 09:30:24 +0800

    增加zz上车

--- JoinPool.py
+++ JoinPool.py
@@ -3,25 +3,30 @@ import json                    # Json转换库
 import os
 
 poolurls = ['http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5lZRSjm9nn4J/',
-        'http://api.turinglabs.net/api/v1/jd/jxfactory/create/R0xgDZpumLgWjxq6jCH9Mg==/',
-        'http://api.turinglabs.net/api/v1/jd/farm/create/c9cfb4f9c5634c0dabd3a328a5230f87/',
-        'http://api.turinglabs.net/api/v1/jd/bean/create/twfaqwk2rzgw35t66avsmxlb6u/',
-        'http://api.turinglabs.net/api/v1/jd/pet/create/MTE1NDUyMjEwMDAwMDAwMzM4MTUyNDc=/']
+            'http://api.turinglabs.net/api/v1/jd/jxfactory/create/R0xgDZpumLgWjxq6jCH9Mg==/',
+            'http://api.turinglabs.net/api/v1/jd/farm/create/c9cfb4f9c5634c0dabd3a328a5230f87/',
+            'http://api.turinglabs.net/api/v1/jd/bean/create/twfaqwk2rzgw35t66avsmxlb6u/',
+            'http://api.turinglabs.net/api/v1/jd/pet/create/MTE1NDUyMjEwMDAwMDAwMzM4MTUyNDc=/',
+            'https://code.chiang.fun/api/v1/jd/jdzz/create/AaGwUxujTz3Y/']
 rs_text = ''
 
 def JoinPool(url):
     rslt = requests.get(url)
     # js = rslt.json()['message']
     # js = json.loads(ret)
-    return rslt.json()['message']
+    if url.__contains__('jdzz'):
+        return rslt.json()['msg']
+    else:
+        return rslt.json()['message']
 
 for purl in poolurls:
     if len(rs_text) > 0:
         rs_text = JoinPool(purl) + '\n\n' + rs_text
     if len(rs_text) == 0:
         rs_text = JoinPool(purl) + '\n\n'
-        
+
 def serverJ(title, content):
+    sckey = ''
     if "PUSH_KEY" in os.environ:
         sckey = os.environ["PUSH_KEY"]
 
@@ -35,6 +40,6 @@ def serverJ(title, content):
     }
     response = requests.post(f"https://sc.ftqq.com/{sckey}.send", data=data)
     print(response.text)
-    
-serverJ("JoinPool", rs_text)    
+
+serverJ("JoinPool", rs_text)
 print(rs_text)

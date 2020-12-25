852a04313b9739b25b7629556653c85989ee8800
Sirius2s <Sirius2s@gmail.com>
2020-12-25 09:33:23 +0800

    update

--- JoinPool.py
+++ JoinPool.py
@@ -3,10 +3,13 @@ import json                    # Json转换库
 import os
 
 poolurls = ['http://api.turinglabs.net/api/v1/jd/ddfactory/create/P04z54XCjVWnYaS5lZRSjm9nn4J/',
+            # 'http://api.turinglabs.net/api/v1/jd/ddfactory/create/T0113aQxGVta9xcCjVWnYaS5kRrbA/',
             'http://api.turinglabs.net/api/v1/jd/jxfactory/create/R0xgDZpumLgWjxq6jCH9Mg==/',
             'http://api.turinglabs.net/api/v1/jd/farm/create/c9cfb4f9c5634c0dabd3a328a5230f87/',
             'http://api.turinglabs.net/api/v1/jd/bean/create/twfaqwk2rzgw35t66avsmxlb6u/',
             'http://api.turinglabs.net/api/v1/jd/pet/create/MTE1NDUyMjEwMDAwMDAwMzM4MTUyNDc=/',
+            'https://code.chiang.fun/api/v1/jd/jdzz/create/S3aQxGVta9xc/',
+            'https://code.chiang.fun/api/v1/jd/jdcrazyjoy/create/NTqJ2nWDjUarfc3eWGgXhA==/',
             'https://code.chiang.fun/api/v1/jd/jdzz/create/AaGwUxujTz3Y/']
 rs_text = ''
 

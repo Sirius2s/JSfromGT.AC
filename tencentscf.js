95ee5afb35c79349c0698a154e2c59213598de66
Sirius2s <Sirius2s@gmail.com>
2021-01-29 10:39:42 +0800

    env in test

--- tencentscf.js
+++ tencentscf.js
@@ -134,14 +134,84 @@ const sleep = ms => new Promise(res => setTimeout(res, ms));
   // // console.log(`steps[5] evn is:${vars}`)
 
   // // 2.
+  let L_env = [
+    "SCF_REGION",
+    "JD_COOKIE",
+    "JD_DEBUG",
+    "JD_USER_AGENT",
+    "PUSH_KEY",
+    "BARK_PUSH",
+    "BARK_SOUND",
+    "TG_BOT_TOKEN",
+    "TG_USER_ID",
+    "DD_BOT_TOKEN",
+    "DD_BOT_SECRET",
+    "QYWX_KEY",
+    "IGOT_PUSH_KEY",
+    "QQ_SKEY",
+    "QQ_MODE",
+    "PUSH_PLUS_TOKEN",
+    "PUSH_PLUS_USER",
+    "TG_PROXY_HOST",
+    "TG_PROXY_PORT",
+    "FRUITSHARECODES",
+    "PETSHARECODES",
+    "PLANT_BEAN_SHARECODES",
+    "SUPERMARKET_SHARECODES",
+    "DDFACTORY_SHARECODES",
+    "DREAM_FACTORY_SHARE_CODES",
+    "JDZZ_SHARECODES",
+    "JDJOY_SHARECODES",
+    "BOOKSHOP_SHARECODES",
+    "JD_CASH_SHARECODES",
+    "JXNC_SHARECODES",
+    "JDNIAN_SHARECODES",
+    "JDSXSY_SHARECODES",
+    "JD_BEAN_STOP",
+    "JD_BEAN_SIGN_STOP_NOTIFY",
+    "JD_BEAN_SIGN_NOTIFY_SIMPLE",
+    "PET_NOTIFY_CONTROL",
+    "FRUIT_NOTIFY_CONTROL",
+    "JD_JOY_REWARD_NOTIFY",
+    "JOY_FEED_COUNT",
+    "JOY_HELP_FEED",
+    "JOY_RUN_FLAG",
+    "JOY_TEAM_LEVEL",
+    "JOY_RUN_NOTIFY",
+    "JD_JOY_REWARD_NAME",
+    "MARKET_COIN_TO_BEANS",
+    "MARKET_REWARD_NOTIFY",
+    "JOIN_PK_TEAM",
+    "SUPERMARKET_UPGRADE",
+    "BUSINESS_CIRCLE_JUMP",
+    "SUPERMARKET_LOTTERY",
+    "FRUIT_BEAN_CARD",
+    "UN_SUBSCRIBES",
+    "UN_BIND_CARD_NUM",
+    "UN_BIND_STOP_CARD",
+    "JDJOY_HELPSELF",
+    "JDJOY_APPLYJDBEAN",
+    "BUY_JOY_LEVEL",
+    "MONEY_TREE_SELL_FRUIT",
+    "FACTORAY_WANTPRODUCT_NAME",
+    "JXNCTOKENS",
+    "JD_IMMORTAL_LATLON",
+    "JD_WECHAT_USER_AGENT",
+    "DREAMFACTORY_FORBID_ACCOUNT",
+    "JDFACTORY_FORBID_ACCOUNT"
+  ] 
+
   let vars = []
   for(let key in process.env){
-    if(process.env[key]!=='' && process.env.hasOwnProperty(key) && key!=='PATH' && key!=='STAGE' && key!=='SERVERLESS_PLATFORM_VENDOR' && key!=='TENCENT_SECRET_ID' && key!=='TENCENT_SECRET_KEY' && key!=='TENCENT_REGION' && key!=='TENCENT_FUNCTION_NAME')
+    // if(process.env[key]!=='' && process.env.hasOwnProperty(key) && key!=='PATH' && key!=='STAGE' && key!=='SERVERLESS_PLATFORM_VENDOR' && key!=='TENCENT_SECRET_ID' && key!=='TENCENT_SECRET_KEY' && key!=='TENCENT_REGION' && key!=='TENCENT_FUNCTION_NAME')
+    if(process.env[key]!=='' && key !== 'PATH' && key!=='STAGE' && key!=='SERVERLESS_PLATFORM_VENDOR' && key!=='TENCENT_SECRET_ID' && key!=='TENCENT_SECRET_KEY' && key!=='TENCENT_REGION' && key!=='TENCENT_FUNCTION_NAME')
       vars.push({
         "Key": key,
         "Value": process.env[key]
       })    
     console.log(`all evn is=${key}:${process.env[key]}`)
+    if(key in L_env)
+      console.log(`in L_env is=${key}:${process.env[key]}`)
   }
 
   // // 3.

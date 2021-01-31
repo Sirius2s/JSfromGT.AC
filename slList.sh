50062afd2ccbcdfc8fe2790b083f96f8a83e0c9e
Sirius2s <Sirius2s@Gmail.com>
2021-01-31 23:39:09 +0800

    add slList

---
+++ slList.sh
@@ -0,0 +1,4 @@
+wget -O jd_5g.js https://gitee.com/lxk0301/jd_scripts/raw/master/jd_5g.js
+wget -O jd_818.js https://gitee.com/lxk0301/jd_scripts/raw/master/jd_818.js
+wget -O jd_xgyl.js https://gitee.com/lxk0301/jd_scripts/raw/master/jd_xgyl.js
+wget -O jd_immortal.js https://gitee.com/lxk0301/jd_scripts/raw/master/jd_immortal.js

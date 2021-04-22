8720a8ca404d94e23b13fcb4914d59b1bf99d21f
Sirius2s <Sirius2s@gmail.com>
2021-04-22 15:51:34 +0800

    getcode.sh on

---
+++ cf_getcode.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#change file to get daily sharecode
+#eg: 
+# chmod +x src/tst.sh
+# sh src/tst.sh ./src/activity/jd_carnivalcity.js 互助码】 】
+
+filename=$1	    #参数1:文件路径及名称
+keyword=$2	    #参数2:sharecode所在行识别关键字
+delimiter=$3	#参数3:指定分隔符
+
+echo '文件名: '$1
+echo '关键字: '$2
+echo '分隔符: '$3
+
+sed -i '/let cookiesArr/a\
+\
+var ds_code;\
+console.oldLog = console.log;\
+console.log = function(str) {\
+	console.oldLog(str);\
+    if (str.includes('"'$keyword'"')) {\
+        ds_code = str.split('"'$delimiter'"')[1] ;\
+    }\
+}' $filename
+
+sed -i '/allMessage += `京东账号/i\
+      allMessage += `code is:\\n/submit_activity_codes carnivalcity ${ds_code}\\n`
+' $filename
+
+sed -i '/name, `京东账号/i\
+    message += `code is:\\n${ds_code}\\n`
+' $filename

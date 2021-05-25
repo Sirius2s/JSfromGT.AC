0e2567df11c03bdd5623bbdf9d4064ef8d81918f
Sirius2s <Sirius2s@gmail.com>
2021-05-25 11:55:56 +0800

    update getcode send nfy

--- cf_getcode.sh
+++ cf_getcode.sh
@@ -14,14 +14,15 @@ echo '分隔符: '$3
 
 sed -i '/let cookiesArr/a\
 \
+let Time2hour = new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000;\
 var ds_code;\
 console.oldLog = console.log;\
 console.log = function(str) {\
 	console.oldLog(str);\
     if (str.includes('"'$keyword'"')) {\
         ds_code = str.split('"'$delimiter'"')[1] ;\
-        if (new Date().getHours() < 12){\
-            notify.sendNotify(`code is:\\n/submit_activity_codes carnivalcity ${ds_code}\\n`);\
+        if (new Date(Time2hour).getHours() < 12){\
+            notify.sendNotify($.name,`code is:\\n/submit_activity_codes carnivalcity ${ds_code}\\n`);\
         }\
     }\
 }' $filename

06fdef961653aff0300edb19657221b276050994
Sirius2s <53803920+Sirius2s@users.noreply.github.com>
2021-06-21 22:42:12 +0800

    fix

--- tencentscf.js
+++ tencentscf.js
@@ -71,7 +71,7 @@ const sleep = ms => new Promise(res => setTimeout(res, ms));
 
   console.log(`更新环境变量`)
   // 更新环境变量
-  let inputYML = '/.github/workflows/deploy_tencent_scf.yml';
+  let inputYML = '.github/workflows/deploy_tencent_scf.yml';
   let obj = yaml.load(fs.readFileSync(inputYML, {encoding: 'utf-8'}))
 
   let L_env = [

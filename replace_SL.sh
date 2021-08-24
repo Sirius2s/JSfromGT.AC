9a0f07eb8b9f848830c9a5e446085b304d5ce816
Sirius2s <Sirius2s@gmail.com>
2021-08-24 10:11:18 +0800

    feat(f2): decomp/star_shop off

--- replace_SL.sh
+++ replace_SL.sh
@@ -39,8 +39,6 @@ wget -O jd_wish.js https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd
 wget -O z_ryhxj.js https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_ryhxj.js
 wget -O z_ldhwj.js https://raw.githubusercontent.com/Ariszy/Private-Script/master/JD/zy_ldhwj.js
 wget -O jd_Z4B.js https://raw.githubusercontent.com/star261/jd/main/scripts/jd_productZ4Brand.js
-wget -O jd_decompression.js https://raw.githubusercontent.com/star261/jd/main/scripts/jd_decompression.js
-wget -O jd_star_shop.js https://raw.githubusercontent.com/star261/jd/main/scripts/jd_star_shop.js
 ###
 wget -O cf_getcode.sh https://raw.githubusercontent.com/sirius2s/JSfromGT.AC/main/cf_getcode.sh
 # chmod +x cf_getcode.sh

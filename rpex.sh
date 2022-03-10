764496d79969f259061222e20bdcd620b2a0f414
Sirius2s <Sirius2s@gmail.com>
2022-03-10 08:41:18 +0800

    feat(ex): up 7d id

--- rpex.sh
+++ rpex.sh
@@ -49,6 +49,7 @@ activityIdList = [\
     "5fb9a2dad5604c85b2144b5156c6140d",\
     "26806f48bb934932b939ad7220d6e186",\
     "e33d68e4ec884006b61649f3ca236048",\
+    "3002cc73f9214a519b2734e114e469c8",\
     ]' jd_sevenDay.js
 ###
 sed -i 's/await\sdoHelp()/;/g' `grep 'await\sdoHelp()' -rl ./`

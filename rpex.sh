fd7c0ed4615db328005502f4dc1c6c0222d904a3
Sirius2s <Sirius2s@gmail.com>
2022-05-06 10:36:55 +0800

    fix(ex): wish id

--- rpex.sh
+++ rpex.sh
@@ -17,7 +17,7 @@ wget -O jd_dpqd.js https://raw.githubusercontent.com/xdhgsq/xdh/main/js/jd_dpqd.
 wget -O jd_MMdou.js https://raw.githubusercontent.com/gys619/banana1/main/gua_MMdou.js
 # wget -O jd_carnivalcity.js https://raw.githubusercontent.com/JDHelloWorld/jd_scripts/main/jd_carnivalcity.js
 wget -O jd_nzmh.js https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_nzmh.js
-wget -O jd_wish.js https://raw.githubusercontent.com/yuannian1112/jd_scripts/main/jd_wish.js
+wget -O jd_wish.js https://raw.githubusercontent.com/6dylan6/jdpro/main/jd_wish.js
 wget -O jd_esManager.js https://raw.githubusercontent.com/gys619/banana1/main/jd_EsportsManager.js
 wget -O jd_Z4B.js https://raw.githubusercontent.com/gys619/banana1/main/jd_productZ4Brand11.js
 wget -O jd_jieMo.js https://raw.githubusercontent.com/he1pu/JDHelp/main/jd_jieMo.js

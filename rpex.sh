561241b4a8e240a90c930b0c59b51f18d5e50dcf
Sirius2s <Sirius2s@gmail.com>
2023-05-15 09:12:34 +0800

    fix(hw): speedSign

--- rpex.sh
+++ rpex.sh
@@ -8,6 +8,7 @@ wget -O index_hw.js https://raw.githubusercontent.com/Sirius2s/JSfromGT/backup/i
 wget -O jdCookie_hw.js https://raw.githubusercontent.com/Sirius2s/JSfromGT/backup/jdCookie_hw.js
 wget -O config.json https://raw.githubusercontent.com/Sirius2s/JSfromGT/backup/config.json
 wget -O sendNotify.js https://raw.githubusercontent.com/Sirius2s/JSfromGT/backup/sendNotify.js
+wget -O jd_speed_sign.js https://raw.githubusercontent.com/Sirius2s/JSfromGT/backup/jd_speed_sign.js
 wget -O z_ft.js https://raw.githubusercontent.com/Sirius2s/JSfromGT/backup/z_ft.js
 # wget -O jd_m_sign.js https://raw.githubusercontent.com/KingRan/KR/main/jd_m_sign.js
 # wget -O jd_lotteryMachine.js https://raw.githubusercontent.com/gys619/banana1/main/jd_lotteryMachine.js
